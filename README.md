411f338edab7455b5177adffb5ba674ae6da1d52
vbucode <bulatv2@gmail.com>
2023-06-10 18:39:41 +0500

    refactoring

---
+++ README.md
@@ -0,0 +1,3 @@
+# task-manager
+
+task manager - command-line task manager
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,25 @@ use std::io::stdin;
 fn main() {
     let mut v = vec![];
     println!("\ntask manager\ntop tasks is most important and urgent\n add - text and enter \n show - show task \n take - take task \n exit - exit \n");
-    let mut line = String::new();
-    println!("> ");
-    let t1 = stdin().read_line(&mut line).unwrap();
-    if line == "show" {
-        println!("\nto do:");
-        for i in v {
-            println!("{:?}", i);
+    loop {
+        let mut line = String::new();
+        println!("> ");
+        let t1 = stdin().read_line(&mut line).unwrap();
+        if line == "show\n" {
+            println!("\nto do:");
+            for (x, i) in v.iter().enumerate() {
+                println!("{:?}) {:?}", x + 1, i);
+            }
         }
-    }
-    else if line == "take" {
-        println!("{:?} is done.", v[0]);
-        v.pop();
-    }
-    else {
-        v.push(line);
+        else if line == "take\n" {
+            println!("{:?} is done.", v[0]);
+            v.pop();
+        }
+        else if line == "exit\n" {
+            break;
+        }
+        else {
+            v.push(line);
+        }
     }
 }
