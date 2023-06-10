0c9191adbbcbcc8208b7a9f0f9dd73eb7173aecc
vbucode <bulatv2@gmail.com>
2023-06-10 17:35:14 +0500

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,21 @@
 use std::io::stdin;
 fn main() {
-    let mut v:Vec<_> = vec![];
-    println!("Hello, world!\ntask manager\ntop tasks is most important and urgent\n add - text and enter \n show - show task \n take - take task \n exit - exit \n");
+    let mut v = vec![];
+    println!("\ntask manager\ntop tasks is most important and urgent\n add - text and enter \n show - show task \n take - take task \n exit - exit \n");
     let mut line = String::new();
     println!("> ");
     let t1 = stdin().read_line(&mut line).unwrap();
-    corefunction(&mut line, &mut v);
-    fn corefunction<T>(arg:&mut String, v:&mut Vec<T>) {
-        if arg == "show" {
-            println!("\nto do:");
-            for i in v.len() {
-                println!("{}) {}", i + 1, v[i]);
-            }
+    if line == "show" {
+        println!("\nto do:");
+        for i in v {
+            println!("{:?}", i);
         }
-        else if arg == "take" {
-            println!("{} is done.", v[0]);
-        }
-        else {
-            v.push(arg);
-        }
+    }
+    else if line == "take" {
+        println!("{:?} is done.", v[0]);
+        v.pop();
+    }
+    else {
+        v.push(line);
     }
 }
