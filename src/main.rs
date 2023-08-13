7141f8423fab3bb4ecdd9a0837b82b6e67b64862
vbucode <bulatv2@gmail.com>
2023-08-14 04:07:57 +0500

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
+use std::io;
 use std::io::stdin;
+use std::io::Write;
+
 fn main() {
     let mut v = vec![];
     println!("\ntask manager\ntop tasks is most important and urgent\n add - text and enter \n show - show task \n take - take task \n exit - exit \n");
     loop {
+        print!("enter: ");
+        io::stdout().flush();
         let mut line = String::new();
-        println!(">>> ");
         stdin().read_line(&mut line).unwrap();
-        if line == "show\n" {
+        if line.trim_end() == "show" {
             println!("\nto do:");
             for (x, i) in v.iter().enumerate() {
                 println!("{:?}) {:?}", x + 1, i);
             }
         }
-        else if line == "take\n" {
+        else if line.trim_end() == "take" {
             println!("{:?} is done.", v[0]);
             v.remove(0);
         }
-        else if line == "exit\n" {
+        else if line.trim_end() == "exit" {
             break;
         }
         else {
